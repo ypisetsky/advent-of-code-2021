532fbec6ddfbd0df1e7f6488b6d3ff2afad4487f
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2021-12-04 22:12:04 -0800

    [day 5] Use defaultdict instead of 2d array

--- vents.py
+++ vents.py
@@ -1,33 +1,20 @@
 from util import getlines, tokenedlines
+from collections import defaultdict
+
+def to_int_pair(s):
+    return [int(tok) for tok in s.split(',')]
+def my_range(low, high):
+    return range(high - low + 1)
 
 #lines = getlines("5small")
 lines = getlines("5")
+
 coord_pairs = []
 for line in lines:
     parts = line.split()
-    def to_int_pair(s):
-        toks = s.split(',')
-        return [int(tok) for tok in toks]
-
     coord_pairs.append(sorted([to_int_pair(parts[0]), to_int_pair(parts[2])]))
 
-
-max_x = 0
-max_y = 0
-for s,d in coord_pairs:
-    if s[0] > max_x:
-        max_x = s[0]
-    if d[0] > max_x:
-        max_x = d[0]
-    if s[1] > max_y:
-        max_y = s[1]
-    if d[1] > max_y:
-        max_y = d[1]
-
-data = [[0 for i in range(max_y + 1)] for j in range(max_x + 1) ]
-
-def my_range(low, high):
-    return range(high - low + 1)
+data = defaultdict(lambda: defaultdict(int))
 
 for s, d in coord_pairs:
     if s[0] == d[0]:
@@ -40,13 +27,15 @@ for s, d in coord_pairs:
     elif s[1] - d[1] == s[0] - d[0]:
         for i in my_range(s[0], d[0]):
             data[s[0] + i][s[1] + i] += 1
+            pass
     elif s[1] - d[1] == d[0] - s[0]:
         for i in my_range(s[0], d[0]):
             data[s[0] + i][s[1] - i] += 1
+            pass
 
 ret = 0
-for row in data:
-    for value in row:
+for row in data.values():
+    for value in row.values():
         if value > 1:
             ret += 1
 print(f"Part 2 is {ret}")
