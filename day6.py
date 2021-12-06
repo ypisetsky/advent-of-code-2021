88f7423ca56e828ea07c46a7a587b078304f6464
Yuliy Pisetsky <yuliy.pisetsky@gmail.com>
2021-12-05 22:48:02 -0800

    [day 6] Rename variables

--- day6.py
+++ day6.py
@@ -4,20 +4,21 @@ from collections import defaultdict
 
 data = getlines("6")
 fish = data[0].split(',')
-bucketed = defaultdict(int)
+fish_by_day = defaultdict(int)
 
 for f in fish:
-    bucketed[int(f)] += 1
-def step(fish):
+    fish_by_day[int(f)] += 1
+
+def step(fish_by_day):
     new = {}
     for i in range(1, 9):
-        new[i-1] = fish.get(i, 0)
-    new[8] = fish[0]
-    new[6] += fish[0]
+        new[i-1] = fish_by_day.get(i, 0)
+    new[8] = fish_by_day[0]
+    new[6] += fish_by_day[0]
     return new
 
 ret = 0
 
 for i in range(256):
-    bucketed = step(bucketed)
-    print(f"{i + 1}: {sum(bucketed.values())}")
+    fish_by_day = step(fish_by_day)
+    print(f"{i + 1}: {sum(fish_by_day.values())}")
